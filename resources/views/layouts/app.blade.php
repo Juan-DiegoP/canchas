0605cb10cd8f9b725a5894f2e5efa0dae9a2f60d
Juan Diego <perezlopezjuandiego59@gmail.com>
2026-06-22 14:11:24 -0500

    actualizo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,7 +100,7 @@
     </div>
 
     <footer class="bg-dark text-light text-center py-3 mt-5">
-        <small>&copy; {{ date('Y') }} CanchasYa — Proyecto académico</small>
+        <small>&copy; {{ date('Y') }} CanchasYa </small>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
